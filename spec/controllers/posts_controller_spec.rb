627f649b2233bd6cbbb7a1aca0edcc8e8c429525
Dyego Rodrigo <dyego9444@hotmail.com>
2015-11-22 22:53:27 -0200

    posts controller tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 
 describe PostsController, :type => :controller do
+   describe 'GET #index' do
+      it "should assign posts instance variable to all created posts" do
+         get :index
+         expect(assigns(:posts)).to eq(Post.all)
+      end
+   end
+   describe 'GET #new' do
+      it "should assign post instance variable to a new post" do
+          get :new
+          expect(assigns(:post)).to be_a_new(Post)
+      end
+   end
    describe 'POST #create' do
       context "with correct attributes" do
          it "should create a new post" do
@@ -14,5 +26,11 @@ describe PostsController, :type => :controller do
          end
       end
    end
+   describe "GET #show" do
+      it "should get the first post " do
+          #TODO: Finish it.
+          #get :show, id: 1
+          #expect(assigns(:post)).to eq(Post.find(1))
+      end
+   end
 end
- 
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :post do
+    id 1
     title "Foobar"
     body "Content foobar"
     tag "tech"
