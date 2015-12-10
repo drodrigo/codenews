e010909e77d7701f7127b71a1ac02a71ca27ca81
Dyego Rodrigo <dyego9444@hotmail.com>
2015-12-10 00:02:48 -0200

    creating user model/controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121031053) do
+ActiveRecord::Schema.define(version: 20151210014118) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20151121031053) do
     t.string   "tag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "image"
+    t.string "image64"
   end
 
 end
