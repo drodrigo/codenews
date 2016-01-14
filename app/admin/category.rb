f8f593ddcc2cc755e03e434602d2b0ca2a9b337a
Dyego Rodrigo <dyego9444@hotmail.com>
2016-01-13 22:22:18 -0200

    fix bug: adding new categories through admin panel

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,17 +1,13 @@
 ActiveAdmin.register Category do
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
+  permit_params :name, :remote_image_url
 
+  form do |f|
+    f.inputs "Category Details", :multipart => true do
+      f.input :name
+      f.input :remote_image_url
+    end
+    f.actions
+  end
 
 end
