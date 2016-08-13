59a7e6ab009c13b84c6f12de9bb6d8121c437f0c
Dyego Rodrigo <dyego.lima@modinify.com>
2016-08-13 18:25:35 -0300

    conditional statement for uploading images according to environment

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,7 +7,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.production?
+    storage:fog
+  end
+
   #storage :fog
 
   # Override the directory where uploaded files will be stored.
