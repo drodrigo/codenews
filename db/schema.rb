9033a430c1a4aafdff0b1f4b7a58142fedad2dd4
Dyego Rodrigo <dyego.lima@modinify.com>
2016-08-13 18:16:21 -0300

    using file system instead of s3

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 20160202010432) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.text     "body"
+    t.string   "body"
     t.integer  "category_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
