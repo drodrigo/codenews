18f7610fd45e1e29d2d79fb9c1087ccb6ca7b620
Dyego Rodrigo <dyego.lima@modinify.com>
2016-08-13 18:44:35 -0300

    including .editorconfig file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,34 +1,34 @@
 class PostsController < ApplicationController
-   def index
-      @posts = Post.all
-      @recent_post = Post.order("created_at").last
-   end
+  def index
+    @posts = Post.all
+    @recent_post = Post.order("created_at").last
+  end
 
-   def new
-      @post = Post.new
-   end
+  def new
+    @post = Post.new
+  end
 
-   def create
-      @post =  Post.new(post_params)
-      if @post.save
-         redirect_to @post
-      else
-         redirect_to 'new'
-      end
-   end
+  def create
+    @post =  Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      redirect_to 'new'
+    end
+  end
 
-   def show
-       @post = Post.find(params[:id])
-   end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-   def edit
+  def edit
 
-   end
+  end
 
-   private
-   def post_params
-      params.require(:post).permit(:title, :body, :category_id,
-      :image_thumb, :image_banner, :remote_image_thumb_url,
-      :remote_image_banner_url, :description)
-   end
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :category_id,
+                                 :image_thumb, :image_banner, :remote_image_thumb_url,
+                                 :remote_image_banner_url, :description)
+  end
 end
